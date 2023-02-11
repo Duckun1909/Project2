602c5ee128730cd175074723958363ff407d9a75
DangTay01092003 <124569283+DangTay01092003@users.noreply.github.com>
2023-02-12 00:25:07 +0700

    update 12-2-2023

--- src/main/java/io/aptech/Controller/CategoryController.java
+++ src/main/java/io/aptech/Controller/CategoryController.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -19,6 +20,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CategoryController implements Initializable{
+    @FXML
+    private TextField txtBookId;
+    @FXML
+    private TextField txtBookName;
+    @FXML
+    private TextArea txtDescription;
     @FXML
     private TableColumn<Category, String> cid;
 //    @FXML
@@ -29,6 +36,11 @@ public class CategoryController implements Initializable{
     private TableColumn<Category, String> cdes;
     @FXML
     private TableView<Category> tableCategory = new TableView<Category>();
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnDelete;
 
 
 
@@ -40,10 +52,38 @@ public class CategoryController implements Initializable{
         categories = categoryStatement.getAll();
         System.out.println(categories);
         tableCategory.getColumns().get(0).setVisible(false);
-        cid.setCellValueFactory(new PropertyValueFactory<Category, String>("id"));
+        cid.setCellValueFactory(new PropertyValueFactory<Category, String>("cat_id"));
         cname.setCellValueFactory(new PropertyValueFactory<Category, String>("cat_name"));
         cdes.setCellValueFactory(new PropertyValueFactory<Category,String>("cat_description"));
         tableCategory.setItems(categories);
+
+
+
+        btnDelete.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Alert aler = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to delete", ButtonType.YES, ButtonType.NO);
+                aler.setTitle("Person confirmation");
+                aler.showAndWait();
+                if (aler.getResult().equals(ButtonType.YES)) {
+                    Category c = tableCategory.getSelectionModel().getSelectedItem();
+                    categories.remove(c);
+                }
+            }
+        });
+
+        tableCategory.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Category c = tableCategory.getSelectionModel().getSelectedItem();
+                txtBookId.setText(String.valueOf(c.getCat_id()));
+                System.out.println(c.getCat_id());
+                txtBookId.setDisable(true);
+
+                txtBookName.setText(c.getCat_name());
+                txtDescription.setText(c.getCat_description());
+            }
+        });
 
     }
 }
